0707468a948722b347cf148a0d704803a14f354a
logan-Tolbert <lodev205@gmail.com>
2025-02-06 13:55:44 -0600

    :construction: Add AppData project with SQL data access functionality
    - Add`ISqlDbContext` interface for data operations.
    - Add `SqlDbContext` class using Dapper for loading and saving data.

---
+++ AppData/Context/ISqlDbContext.cs
@@ -0,0 +1,9 @@
+﻿
+namespace AppData.Context
+{
+    public interface ISqlDbContext
+    {
+        IEnumerable<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionName, bool isStoredProcedure = false);
+        void SaveData<T, U>(string sqlStatement, U parameters, string connectionName, bool isStoredProcedure = false);
+    }
+}
