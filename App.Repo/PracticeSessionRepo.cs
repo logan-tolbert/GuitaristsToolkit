11efd877a6b191bd028b2b9d93a536466388f89e
logan-Tolbert <lodev205@gmail.com>
2025-02-07 15:37:32 -0600

    :construction: Add App.Repo project with PracticeSessionRepo class.
    - Add the `PracticeSessionRepo` class to implement repository patterns for managing `PracticeSession` entities, with methods for creating, retrieving, and deleting sessions (not yet implemented).

---
+++ App.Repo/PracticeSessionRepo.cs
@@ -0,0 +1,41 @@
+﻿using App.Data.Context;
+using App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repo
+{
+    public class PracticeSessionRepo
+    {
+        private readonly ISqlDbContext _db;
+
+        public PracticeSessionRepo(ISqlDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PracticeSession> GetAll()
+        {
+            var sql = @"SELECT * FROM PracticeSessions;";
+            return _db.LoadData<PracticeSession, dynamic>(sql, new { }, "Default");
+        }
+
+        public void GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
