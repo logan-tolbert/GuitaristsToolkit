e7789ac2fc8f909c199bc5841debc994ff7c01b9
logan-Tolbert <lodev205@gmail.com>
2025-02-14 11:34:00 -0600

    :recycle: Refactor User Model
    - Add properties `FirstName`, `LastName`, and `PasswordHash`
    - Set default values for `Id` (`Guid.NewGuid()`) and `CreatedAt` (`DateTime.UtcNow`).
     - Add validation attributes for properties
    - Add `string.Empty` for properties of type string: defaults to prevent null values.

--- App.Models/User.cs
+++ App.Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,33 @@ namespace App.Models
 {
     public class User
     {
-        public int Id { get; set; }
-        public string Username { get; set; } = null!;
-        public string Password { get; set; } = null!;
-        public string Email { get; set; } = null!;
+        public Guid id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(75)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(75)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(8)]
+        [MaxLength(100)]
+        public string PasswordHash { get; set; } = string.Empty;
+       
         public DateTime CreatedAt { get; set; }
     }
 }
