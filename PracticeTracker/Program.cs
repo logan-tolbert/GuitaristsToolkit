756ddb76b1c7d3843dc4f2ca12d864f7d7330808
logan-Tolbert <lodev205@gmail.com>
2025-02-15 08:32:23 -0600

    :sparkles: Update shared layout and Home/Index.cshtml view
    - Update `Home/Index.cshtml` to display Login and Register buttons for unauthenticated users.
    - Add Login and Register links to `_Layout.cshtml` when user is logged out.
    - Add Logout link when user is logged in.

--- PracticeTracker/Program.cs
+++ PracticeTracker/Program.cs
@@ -42,6 +42,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=UserHub}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
