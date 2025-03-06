9743f9ecbadb1fc32cdfe8b1467bd8838e1c6cbf
Logan <lodev205@gmail.com>
2025-03-06 04:14:09 -0600

    :truck: Rename project folder and remove outdated project
    - Rename a project folder, but Visual Studio created a new folder instead of renaming the existing project.
    - Remove the older, replaced project to avoid confusion about which version is correct.

--- AppData/Context/ISqlDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace App.Data.Context
-{
-    public interface ISqlDbContext
-    {
-        IEnumerable<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionName = "Default", bool isStoredProcedure = false);
-        void SaveData<T, U>(string sqlStatement, U parameters, string connectionName = "Default", bool isStoredProcedure = false);
-    }
-}
